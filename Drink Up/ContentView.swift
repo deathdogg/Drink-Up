a810ae6e106389607a7991ca6828aed8a7926796
Ricardo Herrera Reyes <proherrera22@gmail.com>
2024-01-12 09:56:26 -0800

    Initial Commit

---
+++ Drink Up/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Drink Up
+//
+//  Created by Ricardo Herrera on 1/12/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Drink Up/Drink_UpApp.swift
@@ -0,0 +1,17 @@
+//
+//  Drink_UpApp.swift
+//  Drink Up
+//
+//  Created by Ricardo Herrera on 1/12/24.
+//
+
+import SwiftUI
+
+@main
+struct Drink_UpApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
